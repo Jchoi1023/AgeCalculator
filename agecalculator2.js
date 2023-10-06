a779eded6fc2a2f01dad264b0c6bcf515ab735ce
Jchoi1023 <jihye1023@gmail.com>
2023-10-05 17:15:48 -0700

    updated

--- agecalculator2.js
+++ agecalculator2.js
@@ -18,30 +18,30 @@ function calculateAge() {
     today.getFullYear < birthDate.getFullYear()
   ) {
     elResult.innerText = 'Choose correct date!';
-  } else if(birthDate === today) {
-   elResult.innerText = `You are zero-year-old.`
-    } else if(today.getFullYear() -1 == birthDate.getFullYear() &&
-    today.getMonth() == birthDate.getMonth() && 
+  } else if (birthDate === today) {
+    elResult.innerText = `You are zero-year-old.`;
+  } else if (
+    today.getFullYear() - 1 == birthDate.getFullYear() &&
+    today.getMonth() == birthDate.getMonth() &&
     today.getDate() == birthDate.getDate()
-    ){
-      elResult.innerText == `You are one year old.`
-    }
-    //생일이 지난 경우.
-    else if (
-      today.getMonth() > birthDate.getMonth() ||
-      (today.getMonth() == birthDate.getMonth() &&
-        today.getDate() >= birthDate.getDate())
-    ) {
-      age = today.getFullYear() - birthDate.getFullYear();
-      elResult.innerText = `You are ${age} years old.`;
-    } else {
-      //생일이 안 지난 경우.
-      age = today.getFullYear() - birthDate.getFullYear() - 1;
-      elResult.innerText = `You are ${age} years old.`;
-    }
-    elResult.innerText = `You are ${age} years old.`;
-    return age;
+  ) {
+    elResult.innerText == `You are one year old.`;
   }
-
+  //생일이 지난 경우.
+  else if (
+    today.getMonth() > birthDate.getMonth() ||
+    (today.getMonth() == birthDate.getMonth() &&
+      today.getDate() >= birthDate.getDate())
+  ) {
+    age = today.getFullYear() - birthDate.getFullYear();
+    elResult.innerText = `You are ${age} years old.`;
+  } else {
+    //생일이 안 지난 경우.
+    age = today.getFullYear() - birthDate.getFullYear() - 1;
+    elResult.innerText = `You are ${age} years old.`;
+  }
+  // elResult.innerText = `You are ${age} years old.`;
+  return age;
+}
 
 elSubmit.addEventListener('click', calculateAge);
